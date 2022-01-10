a5e1d3d3fcb865d611f02f83b2afe7a97c2e7629
derienn <95044400+derienn@users.noreply.github.com>
2022-01-10 14:21:27 +0100

    first save

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,13 +1,36 @@
 def init_board():
     """Returns an empty 3-by-3 board (with .)."""
-    board = []
+    board = [[ '.','.','.' ],[ '.','.','.' ],[ '.','.','.' ]]
     return board
 
 
 def get_move(board, player):
     """Returns the coordinates of a valid move for player on board."""
-    row, col = 0, 0
-    return row, col
+    row = 0
+    col = 0
+    taken_coordinates = set()
+    row_and_col = ()
+    coordinates = input("provide coordinates: ").upper()
+    row = coordinates[0]
+    col = int(coordinates[1])
+    while coordinates not in {"A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"} or row_and_col in taken_coordinates:
+        print("Please enter valid coordinates (A1, A2, A3, B1, B2, B3, C1, C2, C3): ")
+        coordinates = input("provide coordinates: ").upper()
+        row_and_col = (coordinates[0], int(coordinates[1]))
+    # row = input("Row: ").lower()
+    # col = int(input("Col: "))
+    
+    # while row not in {"a", "b", "c"}:
+    #     print("Please enter A, B or C")
+    #     row = input("Row: ").lower()
+    # while col not in {1, 2, 3}:
+    #     print("Please enter 1, 2 or 3")
+    #     col = int(input("Col: "))
+    # row_and_col = (row, col)
+    # while row_and_col not in taken_coordinates:
+    #     print("Those coordinates are already taken.")
+    taken_coordinates.add(row_and_col)
+    return print(row_and_col)
 
 
 def get_ai_move(board, player):
